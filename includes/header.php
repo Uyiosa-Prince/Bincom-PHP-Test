23c3eda2a73ba9518f0961e266214b7da470c918
Prince Uyi <uyimarcel@gmail.com>
2021-07-23 22:48:42 -0400

    changed require_once "process.php" to include "process.php"

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,11 @@
-<?php require_once "process.php"; ?>
+<?php include "process.php"; ?>
 <!DOCTYPE html>
 <html>
     <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bincom PHP Test</title>
-    <!--My Github -> https://github.com/Uyiosa-Prince/To-Do-list -->
+    <!--My Github -> https://github.com/Uyiosa-Prince/Bincom-PHP-Test -->
     <!--stylesheet file Link-->
     <link rel="stylesheet" type="text/css" href="images/style.css"> 
 
