e9389f922015edd4e188bc93710e9455d32f5d99
Prince Uyi <uyimarcel@gmail.com>
2021-07-24 20:31:05 -0400

    Changed the header file in index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php include "includes/header.php" ?>
+<!-- <?php // include "includes/header.php" 
+        ?> -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bincom PHP Test</title>
+    <!--My Github -> https://github.com/Uyiosa-Prince/Bincom-PHP-Test -->
+    <!--stylesheet file Link-->
+    <link rel="stylesheet" type="text/css" href="images/style.css">
+
+    <!--JavaScript file Link-->
+    <script src="images/script.js" language="JavaScript"></script>
+</head>
+
 <body style="text-decoration: none;">
     <ul>
         <li><a href="question1.php">Question1</a></li>
@@ -6,3 +23,5 @@
         <li><a href="question3.php">Question3</a></li>
     </ul>
 </body>
+
+</html>
