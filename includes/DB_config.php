445626afa9efc3eb74cf0b1fc8270ee957a56486
Prince Uyi <uyimarcel@gmail.com>
2021-07-27 14:48:48 -0400

    switched to localhost connection in DB_Config

--- includes/DB_config.php
+++ includes/DB_config.php
@@ -1,11 +1,11 @@
 <?php
 // connect to DB server
 
-//heroku jawsDB Development connection
-$HOST = 'kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-$USER = 'l1oyhfke3y8sr7q8';
-$PASSWORD = 'esdabajrsmrp7w04';
-$DBNAME = 'ya1w5031pkspy3pv';
+//local host Development connection
+$HOST = 'localhost';
+$USER = 'root';
+$PASSWORD = '';
+$DBNAME = 'bincomphptest';
 
 
 $connection = mysqli_connect($HOST, $USER, $PASSWORD, $DBNAME);
@@ -13,12 +13,13 @@ $connection = mysqli_connect($HOST, $USER, $PASSWORD, $DBNAME);
 if ($connection === false) {
     die("Error: Unable to connect".mysqli_connect_error());
 }
-// //local host Development connection
-// $HOST = 'localhost';
-// $USER = 'root';
-// $PASSWORD = '';
-// $DBNAME = 'bincomphptest';
 
+
+// //heroku jawsDB Development connection
+// $HOST = 'kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+// $USER = 'l1oyhfke3y8sr7q8';
+// $PASSWORD = 'esdabajrsmrp7w04';
+// $DBNAME = 'ya1w5031pkspy3pv';
 
 // $connection = mysqli_connect($HOST, $USER, $PASSWORD, $DBNAME);
 
